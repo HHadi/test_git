429ce2c6264f9ed2e9e13ab9fd2e1e539962fb00
HHadi <hhamoud@mail.csuchico.edu>
2013-09-05 11:06:40 -0700

    test git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyTestingProject::Application.config.secret_key_base = 'ba73cd2d421fba417814013fb814fdb86d051c488ba60a3a8e6c01b78bd60bd050e27b6febac19e8823e56013a08f9fbc1d3e562b62130b556aeff7b84f539e7'
